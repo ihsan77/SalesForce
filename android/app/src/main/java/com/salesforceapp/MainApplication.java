c5d7c6d4cc767963f9311fbf536d93ffcd1ee1b4
ihsanullah <ihsanullah.shaikh@royalcyber.com>
2021-11-24 10:53:10 +0500

    sales force configuration in android

--- android/app/src/main/java/com/salesforceapp/MainApplication.java
+++ android/app/src/main/java/com/salesforceapp/MainApplication.java
@@ -10,6 +10,15 @@ import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
+import com.salesforce.marketingcloud.MCLogListener;
+import com.salesforce.marketingcloud.MarketingCloudConfig;
+import com.salesforce.marketingcloud.MarketingCloudSdk;
+import com.salesforce.marketingcloud.messages.inbox.InboxMessageManager;
+import com.salesforce.marketingcloud.notifications.NotificationCustomizationOptions;
+import com.salesforce.marketingcloud.notifications.NotificationManager;
+import com.salesforce.marketingcloud.notifications.NotificationMessage;
+
+
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -43,6 +52,64 @@ public class MainApplication extends Application implements ReactApplication {
   @Override
   public void onCreate() {
     super.onCreate();
+
+
+      // MC With SIT Configuration
+      MarketingCloudSdk.setLogLevel(MCLogListener.VERBOSE);
+      MarketingCloudSdk.setLogListener(new MCLogListener.AndroidLogListener());
+      MarketingCloudSdk.init(this, MarketingCloudConfig.builder() //
+              .setApplicationId(BuildConfig.MC_APPLICATION_ID) // SIT App ID from MC
+              .setAccessToken(BuildConfig.MC_ACCESS_TOKEN) // SIT Access Token from MC
+              .setSenderId(BuildConfig.SENDER_ID) // Firebase ID
+              .setDelayRegistrationUntilContactKeyIsSet(true)
+              .setMarketingCloudServerUrl(BuildConfig.MC_CLOUD_SERVER_URL)// MC Endpoint
+              .setInboxEnabled(true)
+              // .setGeofencingEnabled(true)
+              // ENABLE MARKETING CLOUD FEATURES
+              .setNotificationCustomizationOptions(NotificationCustomizationOptions.create(R.drawable.logo))
+              .build(this), initializationStatus -> Log.d("Initialization Status", initializationStatus.toString()));
+
+      MarketingCloudSdk.requestSdk(marketingCloudSdk -> {
+          marketingCloudSdk.getInboxMessageManager().refreshInbox(b -> {
+              Log.v("Inbox Refresh: ", "" + b);
+          });
+      });
+      MarketingCloudSdk.requestSdk(marketingCloudSdk -> {
+
+          marketingCloudSdk.getNotificationManager().setShouldShowNotificationListener(new NotificationManager.ShouldShowNotificationListener() {
+              @Override
+              public boolean shouldShowNotification(@NonNull NotificationMessage notificationMessage) {
+                  Log.d("Should notification", notificationMessage.toString());
+                  if (notificationMessage.customKeys().get("NotifyType") != null && notificationMessage.customKeys().get("NotifyType").equals("STC")) {
+                      Bundle bundle = new Bundle();
+                      try {
+                          JSONObject data = new JSONObject();
+                          data.put("NotifyType", "STC");
+                          bundle.putString("data", data.toString());
+                      } catch (JSONException e) {
+                          e.printStackTrace();
+                      }
+
+
+                      bundle.putString("id", new Random(System.currentTimeMillis()).nextInt() + "");
+                      bundle.putBoolean("ignoreInForeground", false);
+                      bundle.putString("title", notificationMessage.title());
+                      bundle.putString("message", notificationMessage.alert());
+                      bundle.putString("smallIcon", "logo");
+
+
+                      new RNPushNotificationHelper((Application) getApplicationContext()).sendToNotificationCentre(bundle);
+                      return false;
+                  } else {
+                      return true;
+                  }
+              }
+          });
+
+          // Log.d("SDK State", marketingCloudSdk.getSdkState().toString());
+      });
+
+
     SoLoader.init(this, /* native exopackage */ false);
     initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
   }
